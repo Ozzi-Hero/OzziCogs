2e000fa071328c3315c15834dad01850c154bd7c
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-03-13 03:05:13 +1100

    Update calcs.py

--- MQstats/calcs.py
+++ MQstats/calcs.py
@@ -1,6 +1,7 @@
 from redbot.core import commands
 
 from redbot.core import checks, commands
+from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import box, pagify
 from asyncio import sleep
 
@@ -12,19 +13,19 @@ class Mycog(commands.Cog):
 
         #Might integer collection
         await ctx.send("Please send your might")
-        mightStr = await self.bot.wait_for("message", timeout=120, check=check)
+        mightStr = await bot.wait_for("message", timeout=120, check=check)
         #float conversion and comma removal
         await sleep(0.2)
 
         #HP integer collection
         await ctx.send("Please send your HP")
-        hpStr = await self.bot.wait_for("message", timeout=120, check=check)
+        hpStr = await bot.wait_for("message", timeout=120, check=check)
         #float conversion and comma removal
         await sleep(0.2)
 
         #Defence integer collection
         await ctx.send("Please send your def %")
-        defStr = await self.bot.wait_for("message", timeout=120, check=check)
+        defStr = await bot.wait_for("message", timeout=120, check=check)
         #int conversion and % removal
         #float conversion
 
