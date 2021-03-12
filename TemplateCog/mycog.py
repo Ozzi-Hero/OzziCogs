d70db01afda2f661de58ccd01ac7f6a59df13aac
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-03-13 00:07:52 +1100

    Template Cog

---
+++ TemplateCog/mycog.py
@@ -0,0 +1,9 @@
+
+class Mycog(commands.Cog):
+    """My custom cog"""
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
