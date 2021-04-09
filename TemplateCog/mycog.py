cbb732b557a48012997c9fad389339edff0f30e4
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-09 17:40:11 +1000

    Update mycog.py

--- TemplateCog/mycog.py
+++ TemplateCog/mycog.py
@@ -1,5 +1,4 @@
 from redbot.core import commands
-import discord
 
 class Mycog(commands.Cog):
     """My custom cog"""
@@ -9,8 +8,3 @@ class Mycog(commands.Cog):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
-
-    @commands.command()
-    async def createchannel(self, ctx, name):
-        guild = ctx.message.guild
-        await guild.create_text_channel(name)
