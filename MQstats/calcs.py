e4e83bc187986c0dcac27e8269e335680e2f66ed
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-09 17:40:07 +1000

    Update calcs.py

--- MQstats/calcs.py
+++ MQstats/calcs.py
@@ -1,14 +1,8 @@
 from redbot.core import commands
-import discord
-
-from redbot.core import Config, checks, commands
-from redbot.core.bot import Red
-from redbot.core.utils.chat_formatting import box, pagify
-from asyncio import sleep
 
 class Mycog(commands.Cog):
 
 
     @commands.command()
     async def test(self, ctx, args1, args2, args3):
-        await ctx.send(f"1. {args1}\n2. {args2}\n3. {args3}")
+        await ctx.send("f")
