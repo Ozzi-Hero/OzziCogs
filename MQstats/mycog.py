c848ecece36020302c52308005ffb5acb03a7a6f
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-10 18:52:29 +1000

    Update mycog.py

--- MQstats/mycog.py
+++ MQstats/mycog.py
@@ -2,10 +2,14 @@ from redbot.core import commands
 
 class Mycog(commands.Cog):
 
-
+    @bot.group
     @commands.command()
     async def adcalc(self, ctx, might: int, health: int, defence: int):
         defence = defence / 100
         await ctx.send(might)
         await ctx.send(health)
         await ctx.send(defence)
+
+    @adcalc.command()
+    async def wise(self, ctx):
+        await ctx.send("f")
