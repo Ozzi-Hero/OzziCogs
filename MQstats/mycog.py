79c555081d26ada6ddb73fbef3511bedd00008ee
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-10 19:37:40 +1000

    Delete mycog.py

--- MQstats/mycog.py
+++
@@ -1,8 +0,0 @@
-from redbot.core import commands
-
-class Mycog(commands.Cog):
-
-
-    @commands.command()
-    async def test(self, ctx, test: int):
-        await ctx.send(f"Your number is {test}")
