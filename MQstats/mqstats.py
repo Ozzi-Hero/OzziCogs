4f6a8d66201e1134bfcab1cd423ee5491cd562ff
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-11 23:24:49 +1000

    Update mqstats.py

--- MQstats/mqstats.py
+++ MQstats/mqstats.py
@@ -47,8 +47,8 @@ class MQstats(commands.Cog):
         )
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/782630973358866472/830777511411449877/DRD.png')
         embed.add_field(
-            name = f'**Damage from bubble:**\n{round(dmgActual)}\n\n**Health remaining:**\n{round(healthremaining)}',
-            value = ' ',#round(dmgActual),
+            name = '**Damage from bubble:**',
+            value = round(dmgActual),
             inline = True
         )
         embed.add_field(
