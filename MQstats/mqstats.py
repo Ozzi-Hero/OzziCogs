2fb1db508ecebcbeffabe0bfa33a6b1baae3bb10
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-12 00:33:13 +1000

    Update mqstats.py

--- MQstats/mqstats.py
+++ MQstats/mqstats.py
@@ -33,13 +33,6 @@ class MQstats(commands.Cog):
         else:
             adbool = 'No'
 
-        #await ctx.send(f"Damage from bubble: {round(dmgActual):,}")
-        #await ctx.send(f"Health remaining: {round(healthremaining):,}")
-        #await ctx.send(f"Adrenaline threshold: {round(adreThreshold):,}")
-        #await ctx.send(f"Adrenaline activated? {adbool}")
-        #if (adbool == 'No'):
-        #    await ctx.send(f"Required HP decrease to proc adrenaline: {round(adreProcReq)}")
-
         embed = discord.Embed(
             title = 'Optimisation Results',
             description = f'Results for {ctx.author.mention}',
@@ -48,17 +41,17 @@ class MQstats(commands.Cog):
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/782630973358866472/830777511411449877/DRD.png')
         embed.add_field(
             name = '**Damage from bubble:**',
-            value = round(dmgActual),
+            value = f'{round(dmgActual):,}',
             inline = True
         )
         embed.add_field(
             name = '**Health remaining:**',
-            value = round(healthremaining),
+            value = f'{round(healthremaining):,}',
             inline = True
         )
         embed.add_field(
             name = '**Adrenaline threshold:**',
-            value = round(adreThreshold),
+            value = f'{round(adreThreshold):,}',
             inline = False
         )
         embed.add_field(
@@ -70,7 +63,7 @@ class MQstats(commands.Cog):
         if (adbool == 'No'):
             embed.add_field(
                 name = '**Required HP decrease to proc adrenaline:**',
-                value = round(adreProcReq),
+                value = f'{round(adreProcReq):,}',
                 inline = False
             )
         await ctx.send(embed=embed)
