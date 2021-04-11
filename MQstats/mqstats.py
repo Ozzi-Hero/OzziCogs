4a740623bafc722e8ac07e5bd78ab43d998fd6d1
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-11 22:38:30 +1000

    Update mqstats.py

--- MQstats/mqstats.py
+++ MQstats/mqstats.py
@@ -33,19 +33,16 @@ class MQstats(commands.Cog):
         else:
             adbool = 'No'
 
-        await ctx.send(f"Damage from bubble: {round(dmgActual):,}")
-        await ctx.send(f"Health remaining: {round(healthremaining):,}")
-        await ctx.send(f"Adrenaline threshold: {round(adreThreshold):,}")
-        await ctx.send(f"Adrenaline activated? {adbool}")
-        if (adbool == 'No'):
-            await ctx.send(f"Required HP decrease to proc adrenaline: {round(adreProcReq)}")
-
-        member = discord.Member
-        author = ctx.author
+        #await ctx.send(f"Damage from bubble: {round(dmgActual):,}")
+        #await ctx.send(f"Health remaining: {round(healthremaining):,}")
+        #await ctx.send(f"Adrenaline threshold: {round(adreThreshold):,}")
+        #await ctx.send(f"Adrenaline activated? {adbool}")
+        #if (adbool == 'No'):
+        #    await ctx.send(f"Required HP decrease to proc adrenaline: {round(adreProcReq)}")
 
         embed = discord.Embed(
             title = 'Optimisation Results',
-            description = f'Results for {author.mention}',
+            description = f'Results for {ctx.author.mention}',
             colour = discord.Colour.red()
         )
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/782630973358866472/830777511411449877/DRD.png')
