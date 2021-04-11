fd6bfa2f52962a0c76c60d6a2f842c88eb457914
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-11 13:39:08 +1000

    Update mqstats.py

--- MQstats/mqstats.py
+++ MQstats/mqstats.py
@@ -31,13 +31,14 @@ class MQstats(commands.Cog):
         else:
             adbool = 'No'
 
-        await ctx.send(f"Actual damage: {round(dmgActual):,}")
+        await ctx.send(f"Damage from bubble: {round(dmgActual):,}")
         await ctx.send(f"Health remaining: {round(healthremaining):,}")
         await ctx.send(f"Adrenaline threshold: {round(adreThreshold):,}")
         await ctx.send(f"Adrenaline activated? {adbool}")
         if (adbool == 'No'):
-            await ctx.send(f"HP decrease for adrenaline proc: {round(adreProcReq)}")
+            await ctx.send(f"Required HP decrease to proc adrenaline: {round(adreProcReq)}")
 
     @commands.command()
     async def ascalc(self, ctx):
+        """Returns the time taken to finish your last hit combo based off your weapon type and AS"""
         await ctx.send("f")
