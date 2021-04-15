0623b8975add97e8c89a1d3a7a2af23a6edbb072
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-15 18:28:45 +1000

    Update testing.py

--- Testing/testing.py
+++ Testing/testing.py
@@ -3,6 +3,6 @@ from redbot.core import commands
 class Testing(commands.Cog):
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def test(self, ctx):
 
-        await ctx.Message.reply("I can do stuff!")
+        await ctx.message.reply("I can do stuff!")
