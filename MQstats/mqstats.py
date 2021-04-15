e9b14ca60dc9593d7d08f1648698105ea03c2e8b
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-15 21:29:12 +1000

    Update mqstats.py

--- MQstats/mqstats.py
+++ MQstats/mqstats.py
@@ -13,8 +13,6 @@ class MQstats(commands.Cog):
             might = int(str.replace(might, ",", ""))
             health = int(str.replace(health, ",", ""))
             defence = int(str.replace(defence, "%", ""))
-        except (ValueError):
-            return None
         else:
             await ctx.message.reply('Please only use numbers, commas, or "%" signs when you use the command')
 
