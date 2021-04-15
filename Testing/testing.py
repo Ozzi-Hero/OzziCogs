65816815f49ad15cf9c3ce4d5efc439275e5c6d5
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-15 19:25:01 +1000

    Testing for Guild.icon_url

--- Testing/testing.py
+++ Testing/testing.py
@@ -6,9 +6,4 @@ class Testing(commands.Cog):
     @commands.command()
     async def test(self, ctx):
 
-        embed = discord.Embed(
-            title = 'test',
-            value = 'test'
-        )
-
-        await ctx.message.reply(embed=embed)
+        await ctx.message.reply(discord.Guild.icon_url)
