733940eab82ba3f3eb747ab060439e4cc9a60835
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-04-15 19:51:25 +1000

    Update testing.py

--- Testing/testing.py
+++ Testing/testing.py
@@ -6,4 +6,4 @@ class Testing(commands.Cog):
     @commands.command()
     async def test(self, ctx):
 
-        await ctx.message.reply(ctx.guild.icon_url)
+        await ctx.message.reply(ctx.author.avatar_url)
