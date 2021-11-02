728ce87f10fcbb31a7e848f64fddbc726fe32627
Ozzi-Hero <78915589+Ozzi-Hero@users.noreply.github.com>
2021-11-03 08:33:39 +1100

    Update khutils.py

--- KHutils/khutils.py
+++ KHutils/khutils.py
@@ -39,5 +39,5 @@ class KHutils(commands.Cog):
             inline = True
             )
 
-        channel = commands.get_channel(903341457178509373)
+        channel = bot.get_channel(903341457178509373)
         await channel.send(guild, embed=summonEmbed)
